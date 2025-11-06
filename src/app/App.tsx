36e1738eed9c76a1cc481a63767380f079eccb19
khg9900 <whale4100@gmail.com>
2025-11-06 13:19:12 +0900

    [FEAT] 캘린더 보기 페이지 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import LandingPage from "@/pages/LandingPage";
 import LoginPage from "@/pages/LoginPage";
 import FolderListPage from "@/pages/FolderListPage";
 import FolderDetailPage from "@/pages/FolderDetailPage";
-// import CalendarPage from "@/pages/CalendarPage";
+import CalendarPage from "@/pages/CalendarPage";
 // import SchedulePage from "@/pages/SchedulePage";
 // import ScheduleEditPage from "@/pages/ScheduleEditPage";
 import CreateTripPage from "@/pages/CreateTripPage";
@@ -16,7 +16,7 @@ export default function App() {
         <Route element={<AppLayout />}>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
-          {/* <Route path="/calendar" element={<CalendarPage />} /> */}
+          <Route path="/calendar" element={<CalendarPage />} />
           <Route path="/folders" element={<FolderListPage />} />
           <Route path="/folders/:id" element={<FolderDetailPage />} />
           <Route path="/trips/new" element={<CreateTripPage />} />
