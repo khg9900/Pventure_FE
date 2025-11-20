a0a32d96e753c0d28646f662ea04e68eaf295cc0
rabitis99 <kyungsu9914@gmail.com>
2025-11-20 11:08:52 +0900

    refactor(schedule): ScheduleItemCard를 Header/Detail로 분리 및 Mock 개선 - Mock 데이터를 개선하여 변경 사항과 UI 차이를 명확하게 확인 가능

--- src/features/schedule/mock/schedule.mock.ts
+++ src/features/schedule/mock/schedule.mock.ts
@@ -26,11 +26,20 @@ export const sampleSchedules: ScheduleResponseDto[] = [
     isCompleted: false,
     memo: "오후에 가면 더 좋다 🍣",
   },
-    {
+  {
     id: 4,
+    day: 1,
+    sequence: 4,
+    timeSlot: "저녁식사",
+    isCompleted: false,
+    memo: "저녁 식사는 마라탕? 탕탕후루후루 🥘",
+  },
+  {
+    id: 5,
     day: 1,
     sequence: 4,
     timeSlot: "밤",
     isCompleted: false,
   },
+
 ];
